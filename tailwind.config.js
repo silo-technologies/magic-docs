00b937ce98e080a52735063246ebfa5bc4f328dc
Eric Gratta <eric.gratta@gmail.com>
2021-08-28 11:53:50 -0400

    Upgrade all dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
