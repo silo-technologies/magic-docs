7add51dbc17360243c9a94898b535da31e416764
Eric Gratta <eric.gratta@gmail.com>
2021-08-28 12:46:36 -0400

    Add prettier formatting

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// See https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true, // Use single quotes by default
+  jsxSingleQuote: false, // JSX wll use double quotes
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid'
+};
